96a400963c15bd6132c63092c5c92a34b2e2309e
shawon <shawonislam121009@gmail.com>
2021-09-14 06:37:03 +0600

    added the basic model

--- app_history/models.py
+++ app_history/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class SearchHistory(models.Model):
+    user = models.CharField(max_length=100)
+    keyword = models.CharField(max_length=100)
+    created_at = models.DateField()
+    result = models.TextField()
