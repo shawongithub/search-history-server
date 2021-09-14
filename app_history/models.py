da09420a500fdcbd16c1b717322c4895822fc4f9
shawon <shawonislam121009@gmail.com>
2021-09-14 07:14:15 +0600

    added serializer

--- app_history/models.py
+++ app_history/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class SearchHistory(models.Model):
-    user = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     keyword = models.CharField(max_length=100)
     created_at = models.DateField()
     result = models.TextField()
+
+    def __str__(self):
+        return f'{self.user.username} searched {self.keyword}'
