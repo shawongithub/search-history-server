f16acd391749a00148e76c8b68662625bfc05a71
shawon <shawonislam121009@gmail.com>
2021-09-15 00:04:47 +0600

    solved username related issue

--- app_history/serializers.py
+++ app_history/serializers.py
@@ -4,7 +4,10 @@ from . models import SearchHistory
 
 
 class SearchHistorySerializer(serializers.ModelSerializer):
-
+    user = serializers.SlugRelatedField(
+            read_only=True,
+            slug_field='username'
+    )
     class Meta:
         model = SearchHistory
         fields = '__all__'
--- app_history/views.py
+++ app_history/views.py
@@ -10,14 +10,21 @@ from . serializers import SearchHistorySerializer
 
 class UsersSearchHistory(APIView):
 
-
     def get(self, request, format=None):
+        keyword = request.GET.get('keyword')
+        print(type(keyword))
+
         try:
             histories = SearchHistory.objects.all()
+            if keyword != None:
+                histories = [obj for obj in histories if obj.keyword==keyword]
+                print(histories)
+            for obj in histories:
+                print(obj.user)
+
             serializer = SearchHistorySerializer(histories, many=True)
-            data = {
-                        'results': serializer.data
-                    }
-            return Response(data=data,status=status.HTTP_200_OK)
+            result = serializer.data
+            print(result)
+            return Response(data=result,status=status.HTTP_200_OK)
         except:
             return Response(data={'errors': 'Bad Request Format'}, status=status.HTTP_400_BAD_REQUEST)
