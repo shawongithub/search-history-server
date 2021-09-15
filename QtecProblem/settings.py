1db4e1be7e663c6a362c22576a09e6fca017bc4c
shawon <shawonislam121009@gmail.com>
2021-09-15 19:38:53 +0600

    staticfiles stuff added

--- QtecProblem/settings.py
+++ QtecProblem/settings.py
@@ -117,8 +117,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
